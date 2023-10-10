455801fd3fc598fe5cc74255170c3cb0442ad3ca
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2023-10-09 21:36:51 -0400

    2nd change directly on github

--- Hello Master Builders/Hello Master Builders.cpp
+++ Hello Master Builders/Hello Master Builders.cpp
@@ -7,6 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "Change made directly on Github\n";
+    std::cout << "A 2nd change made on Github\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
