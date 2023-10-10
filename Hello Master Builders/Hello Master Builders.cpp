f22a7b4a458918a7348d7083ce8a8cf5873dc0d1
jfduedu <144645190+jfduedu@users.noreply.github.com>
2023-10-10 15:32:04 -0400

    change 1

--- Hello Master Builders/Hello Master Builders.cpp
+++ Hello Master Builders/Hello Master Builders.cpp
@@ -9,6 +9,7 @@ int main()
     std::cout << "Change made directly on Github\n";
     std::cout << "world\n";
     std::cout << "A 2nd change made on Github - now merged\n";
+    std::cout << "Yet another change\n";
     std::cout << "Trevor's change made on prof's local pc\n";
 }
 
