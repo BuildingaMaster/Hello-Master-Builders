cbf4a026a3623bec02ff85d2f551a3833db86005
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2023-10-09 21:35:39 -0400

    Trevor's change on prof's pc

--- Hello Master Builders/Hello Master Builders.cpp
+++ Hello Master Builders/Hello Master Builders.cpp
@@ -7,6 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "Change made directly on Github\n";
+    std::cout << "Trevor's change made on prof's local pc\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
