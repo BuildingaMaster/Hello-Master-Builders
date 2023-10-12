c73d9db3df5cd1e7f863bbf96054555bf2929012
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2023-10-11 20:38:43 -0400

    Mbuild-52 rebase take 2

--- Hello Master Builders/Hello Master Builders.cpp
+++ Hello Master Builders/Hello Master Builders.cpp
@@ -16,6 +16,7 @@ int main()
     std::cout << "Make a change on Master\n";
     std::cout << "Make a rebase prep change on mbuild-52\n";
     std::cout << "Make a 2nd change on Master\n";
+    std::cout << "Rebase mbuild-52 take 2\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
